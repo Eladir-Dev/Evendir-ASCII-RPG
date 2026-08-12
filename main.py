ab511fa5871a344612ca893c67c358a8581bf993
Eladir-Dev <ejmatosperez@gmail.com>
2026-08-12 09:22:12 -0400

    The clear and draw functions were modified and a load function was created

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import os
 
+# State variables
 run = True
 menu = True
 play = False
 show_rules = False
 
-# Stats
+# Larg strings
+rules = "This are the rules"
+
+# Player Stats
+name = "Player"
 HP = 50
 ATK = 3
 
+# Utilitie functions
 def clear():
-    os.system("cls")
+    """
+    This method clears the screen
+    """
+    # Moves cursor to home position and clears the screen
+    print("\033[H\033[2J", end="")
 
-def draw(length :int, char :str):
+def draw(length=10, line_char="-", start="+", finish="+"):
     """
     This method draws a line
     """
+    print(start, end="")
     for i in range(length):
-        print(char, end="")
+        print(line_char, end="")
     print()
+    print(finish, end="")
 
 def save():
+    """
+    This method saves the game to a file
+    """
     list[
         name,
         str(HP),
@@ -33,16 +48,26 @@ def save():
         f.write(item + "\n")
     f.close()
 
-rules = "This are the rules"
+def load():
+    """
+    This method loads the game from a file
+    """
+    f = open("load.txt", "r")
+    load_list = f.readlines()
+    name = load_list[0][:-1]
+    HP = load_list[1][:-1]
+    ATK = load_list[2][:-1]
+    f.close()
 
+# Main loop
 while run:
     while menu:
         clear()
-        Print("""
-        1, New Game
-        2, Load Game
-        3, Rules
-        4, Quit Game""")
+        print("""
+1, New Game
+2, Load Game
+3, Rules
+4, Quit Game""")
 
         if show_rules:
             print(rules)
@@ -57,12 +82,13 @@ while run:
             menu = False
             play = True
         elif choice == "2":
-            f = open("load.txt", "r")
-            load_list = f.readlines()
-            name = load_list[0][:-1]
-            HP = load_list[1][:-1]
-            ATK = load_list[2][:-1]
-            f.close()
+            load()
+            # f = open("load.txt", "r")
+            # load_list = f.readlines()
+            # name = load_list[0][:-1]
+            # HP = load_list[1][:-1]
+            # ATK = load_list[2][:-1]
+            # f.close()
             clear()
             print(f"Welcome back, {name}!")
             input("> ")
